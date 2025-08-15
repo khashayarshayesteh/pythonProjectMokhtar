cea44100b3f4ea9f57cd6c0e7550913d472d401e
khashayar shayesteh <shayesteh.office@gmail.com>
2025-08-14 22:16:23 -0700

    first commit

---
+++ day01/hello_world.py
@@ -0,0 +1,15 @@
+"""
+
+
+these are commented
+"""
+from pkg_resources import non_empty_lines
+
+#print("Hello\nworld")
+#print("\tpython progremming")
+
+#name = non_empty_lines('Hello world')
+#print(name)
+
+name = None
+print(type(name))
