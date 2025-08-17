b4bc40cf7e950cef8a648dfbcb4ec5a4cb956a28
khashayar shayesteh <shayesteh.office@gmail.com>
2025-08-17 13:13:48 -0700

    commit

--- day02/loops.py
+++ day02/loops.py
@@ -1,6 +1,21 @@
 from operator import indexOf
-
+"""
 s = 'Python'
 for each in s :
     print(each)
     print(indexOf(s, each))
+"""
+print ('--------------------------------------------')
+
+num = int(input("Enter a positive number:  "))
+while num <= 0 :
+    num = int(input("Enter a positive number:  "))
+print(f" you inter the {num}  ")
+
+print ('-------------------------------------------')
+
+answer = input('inter yes or no:')
+while not(answer.lower() == 'yes' or answer.lower() == 'no'):
+    answer = input('inter yes or no:')
+
+print(f"you have entered {answer}")
